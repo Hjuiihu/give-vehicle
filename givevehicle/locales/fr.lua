df8317464dcca53d9f52032e8e7f1d8222342b68
Alexis <83204744+Hjuiihu@users.noreply.github.com>
2021-10-05 13:37:21 +0200

    Delete givevehicle directory

--- givevehicle/locales/fr.lua
+++
@@ -1,8 +0,0 @@
-Locales['fr'] = {
-	['gived_car'] = 'Le Véhicule ~y~%s ~s~avec le numéro de plaque ~y~ %s ~s~a été garé dans le ~g~%s~s~\'s garage',
-	['received_car'] = 'Vous avez reçu un véhicule avec le numéro de plaque ~y~%s',
-	['del_car'] = 'A supprimé un véhicule avec numéro le de plaque ~y~%s',	
-	['del_car_error'] = 'nous trouvons pas de véhicule avec la plaque ~y~%s',	
-	['unknown_car'] = '~r~ Modèle de véhicule inconnu ~y~%s',
-	['plate_already_have'] = '~r~ Cette plaque a déjà été utilisée sur un autre véhicule'
-}
